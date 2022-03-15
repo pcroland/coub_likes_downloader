08ba4e6b236087b28970ecde14b5d4c2fb5e587a
pcroland <pcroland@protonmail.ch>
2022-03-16 00:29:11 +0100

    add option to download videos from a specific user

--- README.md
+++ README.md
@@ -6,7 +6,10 @@ pip install -r requirements.txt
 ```
 # Usage
 - download cookies.txt and put it next to the script. (use [Get cookies.txt](https://chrome.google.com/webstore/detail/get-cookiestxt/bgaddhkoddajcdgocldbbfleckgcbcid) for example)
-```
-./coub.py
-```
+- run script.
+
+### Examples
+`./coub.py` (download all liked videos)\
+`./coub.py xyz` (download all videos from user xyz)
+
 ![img](https://i.kek.sh/zjLM3sJzK2h.png)
--- coub.py
+++ coub.py
@@ -5,23 +5,29 @@ import subprocess
 import requests
 from rich import print
 from rich.progress import track
+import sys
 
+if len(sys.argv) == 2:
+    search = f'channel/{sys.argv[1]}'
+else:
+    search = 'likes'
 session = requests.Session()
 session.cookies = http.cookiejar.MozillaCookieJar('cookies.txt')
 session.cookies.load()
 
-print('[bold cyan]Fetching number of pages... [/bold cyan]', end='')
-a = session.get('https://coub.com/api/v2/timeline/likes?all=true&order_by=date&page=1&per_page=25').json()
+print(f'[color(231)]Fetching number of pages of[/color(231)] [magenta]api/v2/timeline/[bold]{search}[/bold][/magenta]: ', end='')
+a = session.get(f'https://coub.com/api/v2/timeline/{search}?page=1&per_page=25').json()
 pages = a['total_pages']
-print(f'[color(231)]{pages}[/color(231)]')
+print(f'[bold cyan]{pages}[/bold cyan]')
 
 url_list = []
-for page in track(range(1, pages + 1), description='Grabbing links... '):
-    page = session.get(f'https://coub.com/api/v2/timeline/likes?all=true&order_by=date&page={page}&per_page=25').json()
+for page in track(range(1, pages + 1), description='[color(231)]Grabbing links... [/color(231)]'):
+    page = session.get(f'https://coub.com/api/v2/timeline/{search}?page={page}&per_page=25').json()
     coubs = page['coubs']
     for coub in coubs:
         url_list.append(coub['permalink'])
+print(f'[color(231)]Number of coubs:[/color(231)] [bold cyan]{len(url_list)}[/bold cyan]')
 
-for url in track(url_list, description='Grabbing videos...'):
+for url in track(url_list, description='[color(231)]Grabbing videos...[/color(231)]'):
     yt_dlp_args = ['yt-dlp', '-o', f'%(upload_date)s_%(title)s_[%(uploader)s] [%(id)s].%(ext)s', f'https://coub.com/view/{url}']
     subprocess.run(yt_dlp_args, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
